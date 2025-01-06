aab568c887eb511326b07c81e333d7dcc056010c
backwardslashn <backwardslashn@gmail.com>
2025-01-06 11:40:04 -0800

    Update Logest-Spec.c

--- Logest-Spec.c
+++ Logest-Spec.c
@@ -43,3 +43,14 @@ int getline(void)
     line[i] = '\0';
     return i;
 }
+
+/* copy: specialized version */
+void copy(void)
+{
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
+}
